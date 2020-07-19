11153076240fe502804279a1db8b2566c739d471
keyou <i@keyou.me>
2020-07-19 15:35:26 +0800

    init repository

---
+++ PeerDesk/MainWindow.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PeerDesk
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.textIp = new System.Windows.Forms.TextBox();
+            this.titleLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.titleLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textIp
+            // 
+            this.textIp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textIp.AutoCompleteCustomSource.AddRange(new string[] {
+            "127.0.0.1"});
+            this.textIp.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textIp.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
+            this.textIp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textIp.Font = new System.Drawing.Font("SimSun", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textIp.Location = new System.Drawing.Point(122, 21);
+            this.textIp.MaxLength = 20;
+            this.textIp.Name = "textIp";
+            this.textIp.Size = new System.Drawing.Size(711, 29);
+            this.textIp.TabIndex = 0;
+            this.textIp.Text = "127.0.0.1";
+            this.textIp.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textIp_KeyUp);
+            // 
+            // titleLayoutPanel
+            // 
+            this.titleLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.titleLayoutPanel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLayoutPanel.BackgroundImage = global::PeerDesk.Properties.Resources.noisy_texture_F9F9F9;
+            this.titleLayoutPanel.ColumnCount = 3;
+            this.titleLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 119F));
+            this.titleLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.titleLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 108F));
+            this.titleLayoutPanel.Controls.Add(this.textIp, 1, 0);
+            this.titleLayoutPanel.Controls.Add(this.label1, 0, 0);
+            this.titleLayoutPanel.Controls.Add(this.connectBtn, 2, 0);
+            this.titleLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.titleLayoutPanel.Name = "titleLayoutPanel";
+            this.titleLayoutPanel.RowCount = 1;
+            this.titleLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.titleLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            this.titleLayoutPanel.Size = new System.Drawing.Size(944, 71);
+            this.titleLayoutPanel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.CausesValidation = false;
+            this.label1.Font = new System.Drawing.Font("SimSun", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(3, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Remote IP";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectBtn.Font = new System.Drawing.Font("SimSun", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.connectBtn.Location = new System.Drawing.Point(839, 19);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(102, 32);
+            this.connectBtn.TabIndex = 2;
+            this.connectBtn.TabStop = false;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.Controls.Add(this.titleLayoutPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "PeerDesk";
+            this.titleLayoutPanel.ResumeLayout(false);
+            this.titleLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textIp;
+        private System.Windows.Forms.TableLayoutPanel titleLayoutPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connectBtn;
+    }
+}
+
---
+++ PeerDesk/MainWindow.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+
+namespace PeerDesk
+{
+    public partial class MainWindow : Form
+    {
+        private const String host_ = "10.23.12.166";//"192.168.147.129";//
+        private const int port_ = 13334;
+        private const int screen_width_ = 1920;
+        private const int screen_height_ = 1080;
+        private Channel channel_ = new Channel();
+        private Dictionary<string, object> cmd_ = new Dictionary<string,object>();
+        private string tmp_file_ = Path.Combine(Path.GetTempPath(), "peerdesk.txt");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClientSize = new Size(screen_width_ / 2, screen_height_ / 2);
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            Debug.WriteLine("handle= {0}", Handle);
+            BeginInvoke(new Action(() =>
+            {
+                if(File.Exists(tmp_file_))
+                {
+                    using (var reader = new StreamReader(tmp_file_))
+                    {
+                        textIp.Text = reader.ReadLine();
+                        textIp.SelectionStart = textIp.Text.Length;
+                        textIp.SelectionLength = 0;
+                    }
+                }
+            }));
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            cmd_.Clear();
+            cmd_.Add("type", "button");
+            cmd_.Add("x", 1.0 * e.X / ClientSize.Width * screen_width_);
+            cmd_.Add("y", 1.0 * e.Y / ClientSize.Height * screen_height_);
+            cmd_.Add("button", e.Button == MouseButtons.Left ? "left" : e.Button == MouseButtons.Right ? "right" : "middle");
+            cmd_.Add("state", "down");
+            var data = channel_.SendCommand(cmd_);
+            Debug.Write(data);
+            this.ActiveControl = null;
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            cmd_.Clear();
+            cmd_.Add("type", "button");
+            cmd_.Add("x", 1.0 * e.X / ClientSize.Width * screen_width_);
+            cmd_.Add("y", 1.0 * e.Y / ClientSize.Height * screen_height_);
+            cmd_.Add("button", e.Button == MouseButtons.Left?"left":e.Button == MouseButtons.Right?"right":"middle");
+            cmd_.Add("state", "up");
+            var data = channel_.SendCommand(cmd_);
+            Debug.Write(data);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            cmd_.Clear();
+            cmd_.Add("type", "move");
+            cmd_.Add("x", 1.0 * e.X / ClientSize.Width * screen_width_);
+            cmd_.Add("y", 1.0 * e.Y / ClientSize.Height * screen_height_);
+            var data = channel_.SendCommand(cmd_);
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            cmd_.Clear();
+            cmd_.Add("type", "scroll");
+            cmd_.Add("x", 0);
+            cmd_.Add("y", e.Delta < 0 ? -5 : 5);
+            var data = channel_.SendCommand(cmd_);
+        }
+
+        private static Dictionary<string, string> key_maps_ = new Dictionary<string, string>() {
+            {"\r","enter"},
+            {"\t","tab"},
+            {"menu","alt"},
+            {"\b","backspace"},
+            {"next","pagedown"},
+            {"lwin","command"},
+            {"rwin","command"},
+        };
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            var key = KeyCodeToUnicode(e.KeyCode);
+            if(key == string.Empty) key = e.KeyCode.ToString().ToLower();
+            if(key_maps_.ContainsKey(key)) key = key_maps_[key];
+            if (key.EndsWith("key")) key = key.Replace("key", string.Empty);
+            if (key.StartsWith("numpad")) key = key.Replace("numpad", string.Empty);
+            if (key.StartsWith("oem")) key = key.Replace("oem", string.Empty);
+            if(Regex.IsMatch(key,"^d\\d+")) key = key.TrimStart('d');
+            cmd_.Clear();
+            cmd_.Add("type", "keyboard");
+            cmd_.Add("key",  key);
+            cmd_.Add("state", "down");
+            //cmd_.Add("modifier", 1.0 * e.Y / ClientSize.Height * screen_height_);
+            var data = channel_.SendCommand(cmd_);
+            Debug.Write(data);
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            var key = KeyCodeToUnicode(e.KeyCode);
+            if (key == string.Empty) key = e.KeyCode.ToString().ToLower();
+            if (key_maps_.ContainsKey(key)) key = key_maps_[key];
+            if (key.EndsWith("key")) key = key.Replace("key", string.Empty);
+            if (key.StartsWith("numpad")) key = key.Replace("numpad", string.Empty);
+            if (key.StartsWith("oem")) key = key.Replace("oem", string.Empty);
+            if (Regex.IsMatch(key, "^d\\d+")) key = key.TrimStart('d');
+            cmd_.Clear();
+            cmd_.Add("type", "keyboard");
+            cmd_.Add("key", key);
+            cmd_.Add("state", "up");
+            //cmd_.Add("modifier", 1.0 * e.Y / ClientSize.Height * screen_height_);
+            var data = channel_.SendCommand(cmd_);
+            Debug.Write(data);
+        }
+        
+        private void textIp_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                connectBtn_Click(sender, e);
+            }
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine("ip: " + textIp.Text);
+                Text = "PeerDesk: Connecting...";
+                if(!channel_.Connect(textIp.Text, port_))
+                {
+                    throw new Exception("Connect failed");
+                }
+                var process = Process.Start("mpv.exe",
+                    string.Format("--wid={0} --autofit-larger={1} tcp://{2}:13333 --no-keepaspect --no-keepaspect-window " +
+                    "--profile=low-latency --video-latency-hacks=yes --no-config --input-vo-keyboard=no --no-input-cursor --idle=yes " +
+                    "--osc=no --no-cache --untimed --no-correct-pts --fps=30 ", Handle, 192 * 7, host_));
+                Text = string.Format("PeerDesk: tcp://{1}:{2} ({0})", Handle, host_, port_);
+                Debug.WriteLine("mpv args: " + process.StartInfo.Arguments);
+                textIp.ReadOnly = true;
+                textIp.Enabled = false;
+                this.ActiveControl = null;
+                connectBtn.Enabled = false;
+                titleLayoutPanel.Enabled = false;
+
+                using (StreamWriter sw = new StreamWriter(tmp_file_))
+                {
+                    sw.WriteLine(textIp.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                Text = "PeerDesk: Connect failed";
+            }
+        }
+
+        #region KeyCodeToUnicode
+        public string KeyCodeToUnicode(Keys key)
+        {
+            byte[] keyboardState = new byte[255];
+            bool keyboardStateStatus = GetKeyboardState(keyboardState);
+
+            if (!keyboardStateStatus)
+            {
+                return "";
+            }
+            // 禁止 shift
+            keyboardState[(int)Keys.ShiftKey] = 0;
+
+            uint virtualKeyCode = (uint)key;
+            uint scanCode = MapVirtualKey(virtualKeyCode, 0);
+            IntPtr inputLocaleIdentifier = GetKeyboardLayout(0);
+
+            StringBuilder result = new StringBuilder();
+            ToUnicodeEx(virtualKeyCode, scanCode, keyboardState, result, (int)5, (uint)0, inputLocaleIdentifier);
+
+            return result.ToString();
+        } 
+
+        [DllImport("user32.dll")]
+        static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll")]
+        static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetKeyboardLayout(uint idThread);
+
+        [DllImport("user32.dll")]
+        static extern int ToUnicodeEx(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pwszBuff, int cchBuff, uint wFlags, IntPtr dwhkl);
+
+        #endregion
+
+
+    }
+
+    class Channel
+    {
+        private TcpClient tcp_client_ = new TcpClient();
+
+        public Channel()
+        {
+            tcp_client_.NoDelay = true;
+            tcp_client_.SendTimeout = 5000;
+            tcp_client_.ReceiveTimeout = 5000;
+        }
+
+        public bool Connect(string host,int port)
+        {
+            if(!tcp_client_.ConnectAsync(host, port).Wait(5000))
+            {
+                tcp_client_.Close();
+                tcp_client_ = new TcpClient();
+                return false;
+            }
+            return true;
+        }
+
+        public string SendCommand(Dictionary<string,object> cmd)
+        {
+            string data = "{";
+            foreach (var item in cmd)
+	        {
+                if (item.Value is string)
+                    data += string.Format("\"{0}\":\"{1}\",", item.Key, item.Value);
+                else if (item.Value is int || item.Value is double)
+                    data += string.Format("\"{0}\":{1},", item.Key, item.Value);
+                else if (item.Value is bool)
+                    data += string.Format("\"{0}\":{1},", item.Key, (bool)item.Value == true ? "true" : "false");
+                else Debug.WriteLine("receive illegal data: {0}",item.Value);
+	        }
+            if (data != "{") data = data.Substring(0, data.Length - 1);
+            data += "}\n";
+
+            try
+            {
+                if (tcp_client_.Connected)
+                    tcp_client_.GetStream().Write(Encoding.ASCII.GetBytes(data), 0, data.Length);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return data;
+        }
+
+    }
+}
